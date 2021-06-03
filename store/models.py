14b829eafaa3dd1d32a83db3ad878c1a3f1211bf
Lance Pidor <kyrios213@gmail.com>
2021-06-03 10:47:54 +0800

    Added querysets

--- store/models.py
+++ store/models.py
@@ -35,7 +35,19 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return str(self.id)
+        return str(self.id)    
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total  
 
 
 class OrderItem(models.Model):
@@ -46,6 +58,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return (f"Order #{self.id}")
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
