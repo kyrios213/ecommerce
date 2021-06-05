84a22e6fb21975d9fc26a2a299520f7a66250bb4
Lance Pidor <kyrios213@gmail.com>
2021-06-05 21:11:14 +0800

    Even more javascript

--- store/views.py
+++ store/views.py
@@ -10,9 +10,6 @@ def updateItem(request):
     productId = data['productId']
     action = data['action']
 
-    print("Action:", action)
-    print("ProductId:", productId)
-
     customer = request.user.customer
     product = Product.objects.get(id=productId)
     order, created = Order.objects.get_or_create(customer=customer, complete=False)
@@ -39,7 +36,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False,}
         cartItems = order['get_cart_total']
 
     products = Product.objects.all()
@@ -58,7 +55,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False,}
         cartItems = order['get_cart_total']
 
     context = {
@@ -74,11 +71,14 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False,}
+        cartItems = order['get_cart_total']
     context = {
         'items': items,
         'order': order,
+        'cartItems': cartItems,
     }
     return render(request, 'store/checkout.html', context)
